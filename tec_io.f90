88152d6b93a02c6d42d3be509f2e6e5b5a0564ec
David Richter <David.Richter.26@nd.edu>
2022-04-25 21:29:16 -0400

    plt file now writes particles. Particles must be collected by and written by root, since tecMPI doesn't work with 1D partitions

--- tec_io.f90
+++ tec_io.f90
@@ -17,18 +17,19 @@ subroutine plt_fields
   implicit none
   include 'mpif.h'
 
-  integer :: i,j,k,kmin,kmax,jmin,jmax
-  integer :: myny,mynz
-  integer :: rankarray(numprocs)
+  integer :: i,j,k,kmin,kmax,jmin,jmax,ip
+  integer :: myny,mynz,mynp,npsize,nps,npe
+  integer :: rankarray(numprocs),nparray(numprocs)
+  integer :: ierr
   integer :: teci,isdouble,filetype,debug,fileformat
   integer :: tecini142,teczne142,tecdat142,tecend142,tecmpiinit142,tecznemap142,tecijkptn142
   character*9 :: output_name
   character*160 :: data_names
   character*1 :: nullchr
-  integer :: sharevar(8),null(8),nodeloc(8),passive(8)
+  integer :: sharevar(10),null(10),nodeloc(10),passive(10)
 
   real,allocatable :: uplt(:,:,:),vplt(:,:,:),wplt(:,:,:),wtmp(:,:,:),tplt(:,:,:),qplt(:,:,:),xplt(:,:,:),yplt(:,:,:),zplt(:,:,:)
-  real*4,allocatable :: toplt(:,:,:)
+  real*4,allocatable :: toplt(:,:,:),xpart(:),ypart(:),zpart(:),upart(:),vpart(:),wpart(:),tpart(:),rpart(:)
 
   path_plt = trim(trim(path_seed)//"tec_viz.szplt")
 
@@ -74,12 +75,13 @@ subroutine plt_fields
   end do
 
 
-  data_names = 'x y z u v w th q'
+  data_names = 'x y z u v w th q tp rp'
 
   nullchr = char(0)
   null = 0
   sharevar = 0
-  passive = 0
+  passive = 1
+  passive(1:8) = 0
   nodeloc = 1
   isdouble = 0
   fileformat = 1
@@ -157,42 +159,89 @@ subroutine plt_fields
 
   !Now get the particle locations to put into the *.plt file:
 
-!  passive = 1
-!  passive(1:7) = 0
-!
-!  teci = teczne142('PART'//nullchr,&
-!       0,&              !0 = ordered data
-!       np,&             !num elements in first direction
-!       1,&              !num elements in second direction
-!       1,&              !num elements in third direction
-!       0,&              !ICellMax (don't use)
-!       0,&              !JCellMax (don't use)
-!       0,&              !KCellMax (don't use)
-!       null,&           !time stamp
-!       0,&              !strandid
-!       0,&              !parent zone
-!       1,&              !0=point, 1=block
-!       0,&              !numfaceconnections
-!       0,&              !faceneighbormode
-!       0,&              !totalnumfacenodes
-!       0,&              !numconnectedboundaryfaces
-!       0,&              !totalnumboundaryconnections
-!       passive,&        !passivevarlist
-!       nodeloc,&        !valuelocation, null means centered
-!       sharevar,&       !sharevarzone, share variable from zone
-!       0)  
-!
-!  teci = tecdat142(np,xp(1:np),isdouble)
-!  teci = tecdat142(np,yp(1:np),isdouble)
-!  teci = tecdat142(np,zp(1:np),isdouble)
-!  teci = tecdat142(np,upart(1:np),isdouble)
-!  teci = tecdat142(np,vpart(1:np),isdouble)
-!  teci = tecdat142(np,wpart(1:np),isdouble)
-!  teci = tecdat142(np,rad(1:np),isdouble)
+  passive = 0
+  passive(7:8) = 1
+
+  mynp = numpart  !From the last time particle number was calculated
+
+  if (myid .eq. 0) then
+     npsize=mynp
+  else
+     npsize=mynp+1
+  end if
+
+  !allocate(xpart(npsize),ypart(npsize),zpart(npsize),upart(npsize),vpart(npsize),wpart(npsize),tpart(npsize),rpart(npsize))
+  allocate(xpart(tnumpart),ypart(tnumpart),zpart(tnumpart),upart(tnumpart),vpart(tnumpart),wpart(tnumpart),tpart(tnumpart),rpart(tnumpart))
+
+
+  !Need the beginning and end index of particle number for each proc
+  call mpi_allgather(mynp,1,mpi_integer,nparray,1,mpi_integer,mpi_comm_world,ierr)
+
+  if (myid .eq. 0) then
+     nps=1
+     npe = mynp
+  else
+    nps = 0
+    do ip = 1,myid
+       nps = nps + nparray(ip) 
+    end do
+    npe = nps + mynp
+  end if
+
+  !call fill_plt_part(npsize,xpart,ypart,zpart,upart,vpart,wpart,tpart,rpart)
+
+  call fill_plt_part_ROOT(nparray,xpart,ypart,zpart,upart,vpart,wpart,tpart,rpart)
+
+  if (myid .eq. 0) then
+
+
+  teci = teczne142('PART'//nullchr,&
+       0,&              !0 = ordered data
+       tnumpart,&       !num elements in first direction
+       1,&              !num elements in second direction
+       1,&              !num elements in third direction
+       0,&              !ICellMax (don't use)
+       0,&              !JCellMax (don't use)
+       0,&              !KCellMax (don't use)
+       null,&           !time stamp
+       0,&              !strandid
+       0,&              !parent zone
+       1,&              !0=point, 1=block
+       0,&              !numfaceconnections
+       0,&              !faceneighbormode
+       0,&              !totalnumfacenodes
+       0,&              !numconnectedboundaryfaces
+       0,&              !totalnumboundaryconnections
+       passive,&        !passivevarlist
+       nodeloc,&        !valuelocation, null means centered
+       sharevar,&       !sharevarzone, share variable from zone
+       0)  
+
+  teci = tecznemap142(1,0)
+
+  !teci = tecijkptn142(myid+1,&
+  !     nps,&
+  !     1,&
+  !     1,&
+  !     npe,&
+  !     1,&
+  !     1)
+
+  teci = tecdat142(tnumpart,xpart,isdouble)
+  teci = tecdat142(tnumpart,ypart,isdouble)
+  teci = tecdat142(tnumpart,zpart,isdouble)
+  teci = tecdat142(tnumpart,upart,isdouble)
+  teci = tecdat142(tnumpart,vpart,isdouble)
+  teci = tecdat142(tnumpart,wpart,isdouble)
+  teci = tecdat142(tnumpart,tpart,isdouble)
+  teci = tecdat142(tnumpart,rpart,isdouble)
+
+  end if
 
   teci = tecend142()  
 
   deallocate(uplt,vplt,wplt,wtmp,tplt,qplt,xplt,yplt,zplt,toplt)
+  deallocate(xpart,ypart,zpart,upart,vpart,wpart,tpart,rpart)
 
 end subroutine plt_fields
 subroutine fill_plt_fields(jmin,jmax,kmin,kmax,uplt,vplt,wtmp,tplt,qplt)
@@ -269,43 +318,151 @@ real :: sendbuf(1:nnx,kmin:kmax,6),recvbuf(1:nnx,kmin:kmax,6)
   end do
 
   end if
-  
-
-!  !Send to left, receive from right
-!  do iz=izs,ize
-!  do ix=1,nnx
-!     sendbuf(ix,iz,1) = u(ix,iys,iz)
-!     sendbuf(ix,iz,2) = v(ix,iys,iz)
-!     sendbuf(ix,iz,3) = w(ix,iys,iz)
-!     sendbuf(ix,iz,4) = e(ix,iys,iz)
-!  end do
-!  end do
-!
-!  do iscl=1,nscl
-!     jloc = 4+iscl
-!     do iz=izs,ize
-!     do ix=1,nnx
-!        sendbuf(ix,iz,jloc) = t(ix,iys,iscl,iz)
-!     end do
-!     end do
-!  end do
-!    
-!  call mpi_sendrecv(sendbuf(1,izs,1),nsend,mpi_real8,yback,0, &
-!                    recvbuf(1,izs,1),nrecv,mpi_real8,yfore,0, &
-!                    mpi_comm_world,istatus,ierr)
-!
-!  do iz=izs,ize
-!  do ix=1,nnx
-!     uplt(ix,iye+1,iz) = recvbuf(ix,iz,1)
-!     vplt(ix,iye+1,iz) = recvbuf(ix,iz,2)
-!     wtmp(ix,iye+1,iz) = recvbuf(ix,iz,3)
-!     tplt(ix,iye+1,iz) = recvbuf(ix,iz,5)
-!     qplt(ix,iye+1,iz) = recvbuf(ix,iz,6)
-!  end do
-!  end do
-  
-
 
 end subroutine fill_plt_fields
+subroutine fill_plt_part(npsize,xpart,ypart,zpart,upart,vpart,wpart,tpart,rpart)
+  use pars
+  use particles
+  implicit none
+  include 'mpif.h'
+
+  integer :: ip,npsize,nsend,nrecv
+  integer :: procnext,procbefore
+  integer :: ierr,istatus(mpi_status_size)
+  real :: sendbuf(8),recvbuf(8)
+  real*4, intent(inout) :: xpart(npsize),ypart(npsize),zpart(npsize),upart(npsize),vpart(npsize),wpart(npsize),tpart(npsize),rpart(npsize)
+
+  !Proc 0 has no "halo", sends its last one to proc 1, etc.
+  if (myid .eq. 0) then
+     ip = 1
+  else
+     ip = 2
+  end if
+
+  part => first_particle
+  do while (associated(part))
+
+    xpart(ip) = part%xp(1)
+    ypart(ip) = part%xp(2)
+    zpart(ip) = part%xp(3)
+    upart(ip) = part%vp(1)
+    vpart(ip) = part%vp(2)
+    wpart(ip) = part%vp(3)
+    tpart(ip) = part%Tp
+    rpart(ip) = part%radius
+     
+    ip = ip+1
+  part => part%next
+  end do
+
+  !Need to get "halo" of particles by sending to procnext
+  procnext = myid+1
+  procbefore = myid-1
+
+  if (myid==0) then
+     procbefore = mpi_proc_null
+  end if
+  if (myid==numprocs-1) then
+     procnext = mpi_proc_null
+  end if
+
+  sendbuf(1) = xpart(npsize)
+  sendbuf(2) = ypart(npsize)
+  sendbuf(3) = zpart(npsize)
+  sendbuf(4) = upart(npsize)
+  sendbuf(5) = vpart(npsize)
+  sendbuf(6) = wpart(npsize)
+  sendbuf(7) = tpart(npsize)
+  sendbuf(8) = rpart(npsize)
+
+  nsend = 8
+  nrecv = nsend
+   
+  !Send to procnext, receive from procbefore
+  call mpi_sendrecv(sendbuf(1),nsend,mpi_real8,procnext,0, &
+                    recvbuf(1),nrecv,mpi_real8,procbefore,0, &
+                    mpi_comm_world,istatus,ierr)
+
+  if (myid .ne. 0) then
+     xpart(1) = recvbuf(1)
+     ypart(1) = recvbuf(2)
+     zpart(1) = recvbuf(3)
+     upart(1) = recvbuf(4)
+     vpart(1) = recvbuf(5)
+     wpart(1) = recvbuf(6)
+     tpart(1) = recvbuf(7)
+     rpart(1) = recvbuf(8)
+   end if
+   
+
+end subroutine fill_plt_part
+subroutine fill_plt_part_ROOT(nparray,xpart,ypart,zpart,upart,vpart,wpart,tpart,rpart)
+  use pars
+  use particles
+  implicit none
+  include 'mpif.h'
+
+  integer :: i,ip,nparray(numprocs),npsarray(numprocs)
+  integer :: ierr
+  real*4, intent(inout) :: xpart(:),ypart(:),zpart(:),upart(:),vpart(:),wpart(:),tpart(:),rpart(:)
+  real*4, allocatable :: myxpart(:),myypart(:),myzpart(:),myupart(:),myvpart(:),mywpart(:),mytpart(:),myrpart(:)
+
+  allocate(myxpart(numpart),myypart(numpart),myzpart(numpart),myupart(numpart),myvpart(numpart),mywpart(numpart),mytpart(numpart),myrpart(numpart))
+
+  !Everyone collects into "my" arrays
+  ip = 1
+  part => first_particle
+  do while (associated(part))
+
+    myxpart(ip) = part%xp(1)
+    myypart(ip) = part%xp(2)
+    myzpart(ip) = part%xp(3)
+    myupart(ip) = part%vp(1)
+    myvpart(ip) = part%vp(2)
+    mywpart(ip) = part%vp(3)
+    mytpart(ip) = part%Tp
+    myrpart(ip) = part%radius
+
+    ip = ip+1
+  part => part%next
+  end do
+
+!  !Start collecting, starting with root
+!  if (myid .eq. 0) then
+!     xpart(1:numpart) = myxpart(1:numpart)
+!     ypart(1:numpart) = myypart(1:numpart)
+!     zpart(1:numpart) = myzpart(1:numpart)
+!     upart(1:numpart) = myupart(1:numpart)
+!     vpart(1:numpart) = myvpart(1:numpart)
+!     wpart(1:numpart) = mywpart(1:numpart)
+!     tpart(1:numpart) = mytpart(1:numpart)
+!     rpart(1:numpart) = myrpart(1:numpart)
+!  end if
+
+  ip = 1
+  do i=1,numprocs
+
+     if (i==1) then
+        npsarray(i) = 0
+     else
+        npsarray(i) = npsarray(i-1)+nparray(i-1)
+     end if
+
+  end do
+
+  call mpi_gatherv(myxpart,numpart,mpi_real4,xpart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(myypart,numpart,mpi_real4,ypart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(myzpart,numpart,mpi_real4,zpart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(myupart,numpart,mpi_real4,upart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(myvpart,numpart,mpi_real4,vpart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(mywpart,numpart,mpi_real4,wpart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(mytpart,numpart,mpi_real4,tpart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+  call mpi_gatherv(myrpart,numpart,mpi_real4,rpart,nparray,npsarray,mpi_real4,0,mpi_comm_world,ierr)
+
+  deallocate(myxpart,myypart,myzpart,myupart,myvpart,mywpart,mytpart,myrpart)
+  
+
+end subroutine fill_plt_part_ROOT
+
 
 end module tec_io
