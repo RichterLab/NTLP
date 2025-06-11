66c968008d81ddaed4747d7d66b9394b610df306
Darius Colangelo <dcolange@epycfe.crc.nd.edu>
2025-06-11 09:54:28 -0400

    Topic: Updated notebooks and clean_data.py
    Description: Made small changes to `data_to_training_file.ipynb` and
    `history_postprocessing.ipynb`. `data_to_training_file.ipynb` should now
    clean data based on quantiles instead of hard coded values. Will write
    `clean_NTLP_data` soon to refactor this notebook and `bin/clean_data.py`
    since they are redundant and `clean_data.py` is more up to date.
    
    `history_postprocessing.ipynb` now plots temperature histograms.

--- droplet_approximation/bin/clean_data.py
+++ droplet_approximation/bin/clean_data.py
@@ -22,7 +22,13 @@ def main( argv ):
     temperature_range = [df["input temperature"].quantile(0.00005),df["input temperature"].quantile(0.99995)]
     radius_range = [1.99e-7, df["input radius"].quantile(0.99995)]
 
-    data = df.loc[(df["output temperature"] > temperature_range[0]) & (df["output temperature"] < temperature_range[1]) & (df["input temperature"] > temperature_range[0]) & (df["input temperature"] < temperature_range[1]) & (df["output radius"] < radius_range[1]) & (df["air density"] == 1.0) & (df["relative humidity"] < 5.0)][["input radius", "input temperature", "salinity", "air temperature", "relative humidity", "air density", "integration time", "output radius", "output temperature"]].to_numpy(dtype=np.float32)
+    data = df.loc[(df["output temperature"] > temperature_range[0]) & 
+            (df["output temperature"] < temperature_range[1]) & 
+            (df["input temperature"] > temperature_range[0]) & 
+            (df["input temperature"] < temperature_range[1]) &
+            (df["output radius"] < radius_range[1]) & 
+            (df["air density"] == 1.0) & 
+            (df["relative humidity"] < 5.0)][["input radius", "input temperature", "salinity", "air temperature", "relative humidity", "air density", "integration time", "output radius", "output temperature"]].to_numpy(dtype=np.float32)
 
     data.tofile(data_save_path)
 
