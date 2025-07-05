6ecc6fc5042ace05060f6f71e00a9abfc2a91f79
Greg Thomsen <gthomsen@gmail.com>
2025-07-04 20:52:47 -0400

    Topic: Python Package Reverting Droplet Parameter Ranges to Original Ranges
    Description:
      * Reverted a previous change that narrowed the droplet parameter ranges
        to match a test pi chamber simulation with 1-way coupling and artificially
        high relative humidity.  That change made it difficult to work on other
        datasets and narrowing ranges for development and testing can now be
        done through the Python package's interface.
    
        While the new ranges are not sufficient for all of the datasets of interest,
        they are closer in size and are acceptable for the time being.

--- droplet_approximation/python/droplet_approximation/physics.py
+++ droplet_approximation/python/droplet_approximation/physics.py
@@ -12,26 +12,13 @@ from scipy.integrate import solve_ivp
 # the small parameters (e.g. radius) as they won't contribute as much as
 # the big parameters (e.g. temperature).  As a result, we use log-scale
 # for the parameters that have a large dynamic range.
-
-# End goal ranges
-
-#DROPLET_RADIUS_LOG_RANGE        = np.array( (-8, -3) )
-#DROPLET_TEMPERATURE_RANGE       = np.array( (273, 310) )
-#DROPLET_SALT_MASS_LOG_RANGE     = np.array( (-22, -10) )
-#DROPLET_AIR_TEMPERATURE_RANGE   = np.array( (273, 310) )
-#DROPLET_RELATIVE_HUMIDITY_RANGE = np.array( (0.65, 1.1) )
-#DROPLET_RHOA_RANGE              = np.array( (0.8, 1.2) )
-#DROPLET_TIME_LOG_RANGE          = np.array( (-2.0, 1.0) )
-
-# NTLP Coupled Ranges (Radius might be unnecesarily large on the bottom end of the range)
-
-DROPLET_RADIUS_LOG_RANGE        = np.array( (-6.75, -3.75) )
-DROPLET_TEMPERATURE_RANGE       = np.array( (284, 300) )
-DROPLET_SALT_MASS_LOG_RANGE     = np.array( (-17.66, -17.65) )
-DROPLET_AIR_TEMPERATURE_RANGE   = np.array( (284, 300) )
-DROPLET_RELATIVE_HUMIDITY_RANGE = np.array( (1.00, 1.11 ) )
-DROPLET_RHOA_RANGE              = np.array( (0.99, 1.01) )
-DROPLET_TIME_LOG_RANGE          = np.array( (-1.4, -0.75) )
+DROPLET_RADIUS_LOG_RANGE        = np.array( (-8, -3) )
+DROPLET_TEMPERATURE_RANGE       = np.array( (273, 310) )
+DROPLET_SALT_MASS_LOG_RANGE     = np.array( (-22, -10) )
+DROPLET_AIR_TEMPERATURE_RANGE   = np.array( (273, 310) )
+DROPLET_RELATIVE_HUMIDITY_RANGE = np.array( (0.65, 1.1) )
+DROPLET_RHOA_RANGE              = np.array( (0.8, 1.2) )
+DROPLET_TIME_LOG_RANGE          = np.array( (-2.0, 1.0) )
 
 def dydt( t, y, parameters ):
     """
