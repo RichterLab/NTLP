Topic: Python Package Avoid Backpropagation for Negative Radii During Early Training Bug Fix
Description:
  * Fixed a critical bug in the architecture and loss function where negative
    radii were predicted, generating NaN normalized radii, and then the loss was
    calculated on the corrupted approximations resulting in failed models.  Now,
    radii NaNs are detected in the loss function and the loss is only calculated
    on the non-NaN samples so that gradients are not corrupted and training can
    continue.

    NOTE: This does not guarantee that the model learns to correctly estimate
          radii residuals, but rather bets that incorrect estimates will
          disappear as the model refines its ability to correctly estimate
          positive radii.  Should a particular model stall and fail to learn
          the ODEs well, simply reinitialize the model and try again.

    NOTE: It is believed an approach similar to this was used during the model's
          initial development but it was lost during development and not
          communicated well prior to introducing this model/loss.

droplet_approximation/python/droplet_approximation/models.py
@@ -360,24 +360,33 @@ def volatile_emotion_l1_loss( normalized_approximations, normalized_outputs ):
 
     """
 
-    # Start with L1.
-    differences = torch.abs( normalized_approximations - normalized_outputs )
+    # Prevent incorporating NaNs into any of the loss calculations, otherwise these
+    # will back propagate and corrupt the model.
+    negative_radii_mask = torch.isnan( normalized_approximations[..., 0] )
+
+    # Start with L1 on the valid approximations.
+    #
+    # NOTE: This prevents updating weights contributing to negative radii
+    #       predictions!!!  This does *NOT* guarantee that the model will
+    #       actually learn, though empirically it seems that simply learning
+    #       valid predictions is sufficent to update the offending weights to
+    #       reduce invalid future predictions.
+    #
+    differences                         = torch.zeros_like( normalized_approximations )
+    differences[:, 1]                   = torch.abs( normalized_approximations[:, 1] -
+                                                     normalized_outputs[:, 1] )
+    differences[~negative_radii_mask, 0] = torch.abs( normalized_approximations[~negative_radii_mask, 0] -
+                                                      normalized_outputs[~negative_radii_mask, 0] )
 
     # Scale to match temperature's magnitude (see Darius, this doesn't make
     # sense to Greg since it's in normalized space).
-    differences[:, 0] /= 25.0
+    differences[~negative_radii_mask, 0] /= 25.0
 
     # Penalize large particles.  Small particles growing or large particles
     # shrinking are penalized heavily, while large particles slightly adjusting
     # don't get penalized as much.
-    differences[:, 0] += torch.abs( 10.0**(2.5 * normalized_approximations[..., 0] - 1.0) - 10.0**(2.5 * normalized_outputs[..., 0] - 1.0) )
-
-    # Convert NaNs to zeros.  This kills the gradient flowing to the weights
-    # contributing to the invalid radii.
-    #
-    # NOTE: This doesn't guarantee removal of NaNs in the backwards graph!
-    #
-    torch.nan_to_num( differences, 0.0 )
+    differences[~negative_radii_mask, 0] += torch.abs( 10.0**(2.5 * normalized_approximations[~negative_radii_mask, 0] - 1.0) -
+                                                       10.0**(2.5 * normalized_outputs[~negative_radii_mask, 0] - 1.0) )
 
     loss = torch.mean( differences )
 