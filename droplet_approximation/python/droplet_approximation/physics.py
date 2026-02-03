70f42dd65fc89895354b02315fb8a7664fbce296
DariusAC <dcolange@nd.edu>
2026-02-03 14:31:56 -0500

    Topic: Python Package Pi Chamber 2-way Parameter Ranges Temporary Change
    Description:
      * The three models on this branch were trained with Pi Chamber 2-way
        ranges. This commit sets the ranges in physics.py accordingly, so
        that when training is run, it is consistent with the parameter
        ranges used for these models.

--- droplet_approximation/python/droplet_approximation/physics.py
+++ droplet_approximation/python/droplet_approximation/physics.py
@@ -31,13 +31,13 @@ DROPLET_DELTA_AIR_TEMPERATURE = 3.0
 # NOTE: We force the data type of each array to avoid NumPy's "help" should
 #       someone specify both ranges as integers.
 #
-DROPLET_RADIUS_LOG_RANGE        = np.array( (-8.0, -3.0),   dtype=np.float64 )
-DROPLET_TEMPERATURE_RANGE       = np.array( (273.0, 310.0), dtype=np.float64 )
-DROPLET_SALT_SOLUTE_LOG_RANGE   = np.array( (-22.0, -8.0),  dtype=np.float64 )
-DROPLET_AIR_TEMPERATURE_RANGE   = np.array( (273.0, 310.0), dtype=np.float64 )
-DROPLET_RELATIVE_HUMIDITY_RANGE = np.array( (0.55, 1.1),    dtype=np.float64 )
-DROPLET_RHOA_RANGE              = np.array( (0.8, 1.3),     dtype=np.float64 )
-DROPLET_TIME_LOG_RANGE          = np.array( (-2.0, 1.0),    dtype=np.float64 )
+DROPLET_RADIUS_LOG_RANGE        = np.array( (-7.0, -4.5),     dtype=np.float64 )
+DROPLET_TEMPERATURE_RANGE       = np.array( (284.0, 295.0),   dtype=np.float64 )
+DROPLET_SALT_SOLUTE_LOG_RANGE   = np.array( (-17.89, -17.87), dtype=np.float64 )
+DROPLET_AIR_TEMPERATURE_RANGE   = np.array( (284.0, 295.0),   dtype=np.float64 )
+DROPLET_RELATIVE_HUMIDITY_RANGE = np.array( (0.98, 1.07),     dtype=np.float64 )
+DROPLET_RHOA_RANGE              = np.array( (0.99, 1.01),     dtype=np.float64 )
+DROPLET_TIME_LOG_RANGE          = np.array( (-1.2, -0.85),    dtype=np.float64 )
 
 # Tolerances for BDF solves.  Per solve_ivp() the effective tolerance is:
 #
